e48735c3b122876e23ab63f0722e6cbbb13c0194
James <jdowns06@gmail.com>
2023-11-21 18:51:30 -0600

    Add booking list view

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,4 +1,3 @@
-#define URL route for index() view
 from django.urls import path
 from . import views
 
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from .models import Menu
-from .serializers import MenuSerializer
-from rest_framework import generics
+from django.contrib.auth.models import User
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
+from rest_framework import generics, viewsets, permissions
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class UserViewSet(viewsets.ModelViewSet):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+   permission_classes = [permissions.IsAuthenticated]
 
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
@@ -14,3 +20,7 @@ class MenuItemsView(generics.ListCreateAPIView):
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
